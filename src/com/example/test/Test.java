15a41c6993252709399ddb651deb07f64647cd14
i.shesholko <i.shesholko@btslogistics.com>
2021-09-20 16:34:33 +0300

    dev1

--- src/com/example/test/Test.java
+++ src/com/example/test/Test.java
@@ -5,6 +5,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("dev1");
         System.out.println("Hello");
+
+
+        System.out.println("dev2");
     }
 
 }
